f05af733330f1b75bfe7d0b8746285a6552cfd81
Bruno Polo <brunopolo@poli.ufrj.br>
2025-02-17 16:44:04 -0300

    fix: remove composition from `File` input, handling file specific logic

--- src/components/Form/Input/File.tsx
+++ src/components/Form/Input/File.tsx
@@ -4,18 +4,48 @@ import React from 'react';
 import { InputProps } from '../../../types/Form';
 import useCurrentForm from '../../../hooks/useCurrentForm';
 
-import Text from './Text';
+import useErrors from '../../../hooks/useErrors';
+import { Obj, Str } from '@luminix/support';
 
 const File: React.FC<InputProps<'file'>> = (props) => {
 
-    const { subscribe } = useCurrentForm();
+    const { subscribe, inputProps, errorBag, } = useCurrentForm();
 
     React.useEffect(() => {
         return subscribe((client) => client.asForm());
     }, [subscribe]);
 
+    const {
+        label, sanitize = (e) => e.target.files,
+        className, ...rest
+    } = props;
+
+    const {
+        [`${Str.camel(props.name)}Error`]: error,
+    } = useErrors(errorBag);
+
     return (
-        <Text {...props} />
+        <>
+            <p>
+                {label && (
+                    <>
+                        <label
+                            className={`luminix-form-label luminix-form-${props.type}-label`.trim()}
+                            htmlFor={props.id}
+                        >
+                            {label}
+                        </label>
+                        <br/>
+                    </>
+                )}
+                <input
+                    className={`luminix-form-input luminix-form-${props.type}-input ${className ?? ''}`.trim()}
+                    {...Obj.omit(inputProps(props.name, sanitize), 'value')}
+                    {...rest}
+                />
+            </p>
+            {error && <p className="luminix-form-error">{error}</p>}
+        </>
     );
 };
 
